91f271a2a41776982cc8ade2efaa84e5e66e0c13
Matheus Chonge <matheus.chong@gmail.com>
2022-04-21 12:46:30 -0300

    Variable Created

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ import cors from 'cors';
 let app = express();
 app.use(cors());
 
+let user = {username: 'user', avatar: 'user-avatar'}
+let tweets = [{username: 'tweets', avatar: 'user-avatar', tweet: 'tweet'}]
+
 app.listen(5000)
