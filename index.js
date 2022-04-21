1a697869cce333ad27c284a88b2830b24a1b0698
Matheus Chonge <matheus.chong@gmail.com>
2022-04-21 20:45:17 -0300

    Base Project Finished

--- index.js
+++ index.js
@@ -3,8 +3,51 @@ import cors from 'cors';
 
 let app = express();
 app.use(cors());
+app.use(express.json())
 
-let user = {username: 'user', avatar: 'user-avatar'}
-let tweets = [{username: 'tweets', avatar: 'user-avatar', tweet: 'tweet'}]
+const users = new Map();
+users.set('Tesla', 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg')
+let tweets = [{ username: 'Tesla', avatar: 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg', tweet: `Let's buy twitter` },
+{ username: 'Tesla', avatar: 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg', tweet: `Let's buy twitter` },
+{ username: 'Tesla', avatar: 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg', tweet: `Let's buy twitter` },
+{ username: 'Tesla', avatar: 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg', tweet: `Let's buy twitter` },
+{ username: 'Tesla', avatar: 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg', tweet: `Let's buy twitter` },
+{ username: 'Tesla', avatar: 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg', tweet: `Let's buy twitter` },
+{ username: 'Tesla', avatar: 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg', tweet: `Let's buy twitter` },
+{ username: 'Tesla', avatar: 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg', tweet: `Let's buy twitter` },
+{ username: 'Tesla', avatar: 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg', tweet: `Let's buy twitter` },
+{ username: 'Tesla', avatar: 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg', tweet: `Let's buy twitter` },
+{ username: 'Tesla', avatar: 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg', tweet: `Let's buy twitter` },
+{ username: 'Tesla', avatar: 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg', tweet: `Let's buy twitter` },
+{ username: 'Tesla', avatar: 'https://i.pinimg.com/originals/7b/80/b3/7b80b3276866f2258b570aa6c07e54ca.jpg', tweet: `Let's buy twitter` }]
+let tweets2 = []
+
+app.post('/sign-up', (req, res) => {
+    const { username, avatar } = req.body;
+    console.log(avatar)
+    //if (users.has(username))
+    users.set(username, avatar)
+    res.send(`OK`)
+})
+
+app.post('/tweets', (req, res) => {
+    const { username, tweet } = req.body;
+    const avatar = users.get(username)
+    tweets.push({username, avatar, tweet})
+    res.send(`OK`)
+})
+
+app.get('/tweets', (req, res) => {
+    let returnedTweets = []
+    const size = tweets.length - 1
+    const start = size > 10 ? size - 10 : 0
+    console.log(`start ${start} size ${size}`)
+    for (let i = start; i < size; i++) {
+        console.log(i)
+        returnedTweets.push(tweets[i])
+    }
+    console.log('cheguei aqui')
+    res.send(returnedTweets)
+})
 
 app.listen(5000)
